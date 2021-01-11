2648569c0068bf91b2816c4be2f35ef195b86b9a
Kenji Saito <ken-yo@mbr.nifty.com>
2021-01-11 17:02:13 +0900

    Replace iron to tide

--- rust-mongodb/src/lib.rs
+++ rust-mongodb/src/lib.rs
@@ -1,55 +1,58 @@
-use mongodb::{Client, options::{ClientOptions, StreamAddress}};
+use futures::stream::StreamExt;
+use bson::{doc, Document};
 use mongodb::error::Result;
-use mongodb::results::{InsertOneResult, DeleteResult};
-use bson::{doc, bson, Document};
+use mongodb::results::{DeleteResult, InsertOneResult};
+use mongodb::{
+    options::{ClientOptions, StreamAddress},
+    Client,
+};
 
 pub fn connect_mongodb() -> Client {
     let options = ClientOptions::builder()
-        .hosts(vec![
-            StreamAddress {
-                hostname: "mongo".into(),
-                port: Some(27017),
-            }
-        ])
+        .hosts(vec![StreamAddress {
+            hostname: "mongo".into(),
+            port: Some(27017),
+        }])
         .build();
 
-    Client::with_options(options)
-        .expect("Failed to initialize standalone client.")
+    Client::with_options(options).expect("Failed to initialize standalone client.")
 }
 
-pub fn create_post(client: &Client, title: &str, body: &str) -> InsertOneResult {
+pub async fn create_post(client: &Client, title: &str, body: &str) -> InsertOneResult {
     let coll = client.database("posts").collection("post");
-    coll.insert_one(doc!{ "title": title, "body": body }, None).unwrap()
+    coll.insert_one(doc! { "title": title, "body": body }, None)
+        .await
+        .unwrap()
 }
 
-pub fn all_posts(client: Client) -> Vec<Document> {
-    let coll = client.database("posts").collection("post");
-    let cursor = coll.find(None, None).unwrap();
-    cursor
-        .filter(|item| {
-            match item {
-                Ok(_) => true,
-                _ => false
-            }
-        })
+pub async fn all_posts(client: Client) -> Vec<Document> {
+    let cursor: mongodb::Cursor = client
+        .database("posts")
+        .collection("post")
+        .find(None, None)
+        .await
+        .unwrap();
+
+    let result = cursor
         .map(|item| {
-            match item {
-                Ok(item) => item,
-                _ => panic!("")
-            }
+            item.unwrap()
         })
-        .collect()
+        .collect();
+
+    result.await
 }
 
-pub fn get_post(client: &Client, title: String) -> Option<Document> {
+pub async fn get_post(client: &Client, title: String) -> Option<Document> {
     let coll = client.database("posts").collection("post");
-    return match coll.find_one(Some(doc!{"title": title}), None) {
-            Ok(item) => item,
-            _ => None
-        };
+    return match coll.find_one(Some(doc! {"title": title}), None).await {
+        Ok(item) => item,
+        _ => None,
+    };
 }
 
-pub fn delete_post(client: &Client, title: String) -> Result<DeleteResult> {
+pub async fn delete_post(client: &Client, title: String) -> Result<DeleteResult> {
     let coll = client.database("posts").collection("post");
-    return coll.delete_one(doc!{"title": title.to_owned()}, None);
+    return coll
+        .delete_one(doc! {"title": title.to_owned()}, None)
+        .await;
 }

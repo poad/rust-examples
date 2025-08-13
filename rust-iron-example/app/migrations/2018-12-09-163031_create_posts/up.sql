827244bf3255249e7730da970a2d7c86c88b92cf
Kenji Saito <ken-yo@mbr.nifty.com>
2025-08-13 12:45:39 +0900

    Bumps crates

--- rust-iron-example/app/migrations/2018-12-09-163031_create_posts/up.sql
+++
@@ -1,9 +0,0 @@
--- Your SQL goes here
-\c diesel_demo
-
-CREATE TABLE posts (
-  id SERIAL PRIMARY KEY,
-  title VARCHAR NOT NULL,
-  body TEXT NOT NULL,
-  published BOOLEAN NOT NULL DEFAULT 'f'
-)
